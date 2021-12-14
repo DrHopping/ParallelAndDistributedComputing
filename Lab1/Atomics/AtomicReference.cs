cfb3ee05772c952e06c69983814020c9488afaad
Mykyta Pedorenko <mpedorenko@corelogic.com>
2021-12-15 00:26:53 +0200

    Add AtomicReference.cs

---
+++ Lab1/Atomics/AtomicReference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Lab1.Atomics
+{
+    public class AtomicReference<T> where T : class {
+        private T _value;
+
+        public AtomicReference(T value){
+            _value = value;
+        }
+
+        public T Value{
+            get{
+                object obj = _value;
+                return (T) Thread.VolatileRead(ref obj);
+            }
+        }
+
+        public bool CompareAndSet(T newValue, T oldValue){
+            return ReferenceEquals(Interlocked.CompareExchange(ref _value, newValue, oldValue), oldValue);
+        }
+        
+        public void Set(T newValue){
+            Interlocked.Exchange(ref _value, newValue);
+        }
+    }
+}
