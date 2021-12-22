1642da4651b80c10e3c1fdd3ef29d52d101be7ca
Mykyta Pedorenko <mpedorenko@corelogic.com>
2021-12-22 13:44:40 +0200

    Add Tests for Task2

---
+++ Lab1.Tests/Task2.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Lab1.Algorithms;
+using Xunit;
+
+namespace Lab1.Tests;
+
+public class Task2
+{
+    [Fact]
+    public void ShouldAddAndRemoveElementsCorrectly()
+    {
+        var numbersToAdd = Enumerable.Range(0, 40).Select(_ => RandomNumberGenerator.GetInt32(0, 100)).Distinct().ToList();
+        var numbersToRemove = Enumerable.Range(0, 20).Select(_ => RandomNumberGenerator.GetInt32(0, 100)).Distinct().ToList();
+        var expected = numbersToAdd.Except(numbersToRemove).OrderBy(x => x).ToList();
+
+        var skipList = new SkipList<int>();
+        Parallel.ForEach(numbersToAdd, x => skipList.Add(x));
+        Parallel.ForEach(numbersToRemove, x => skipList.Remove(x));
+        
+        skipList.Select(x => x).Should().Equal(expected);
+    }
+}
